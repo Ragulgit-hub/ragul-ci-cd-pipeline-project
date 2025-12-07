0b036c5314c3f5bd483937f7b267902fc26ed958
Your Name <you@example.com>
2025-12-07 21:59:27 +0530

    Initial commit - Ragul CI/CD pipeline project

---
+++ src/main/java/com/ragul/cicd/App.java
@@ -0,0 +1,12 @@
+package com.ragul.cicd;
+
+public class App {
+
+    public static String getMessage() {
+        return "Hi, welcome to the Ragul's CI/CD pipeline project!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
